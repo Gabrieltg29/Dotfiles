a5aa480c2ac3fc8b132c9ab87c8bd606eed883aa
gabriel torres <gabrieltggv29@gmail.com>
2024-01-27 20:27:43 -0300

    Adicionado o telescope

--- nvim/lua/manager.lua
+++ nvim/lua/manager.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
    spec = {
      require('plugins.neo_tree'),
-     require('plugins.neodev')
+     require('plugins.neodev'),
+     require('plugins.telescope'),
    },
 })
