2a414191095771755992d97467db9156425b57ca
gabriel torres <gabrieltggv29@gmail.com>
2024-01-30 15:18:58 -0300

    Adicionado o lspconfig

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,15 +1,37 @@
 return {
-  "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-    opts = {
-	ensure_installed = {
-	  "black",
-	  "debugpy",
-	  "mypy",
-	  "ruff",
-	  "pyright",
-	}
-    }
-  end
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+      opts = {
+				ensure_installed = {
+	  			"black",
+	  			"debugpy",
+	  			"mypy",
+	  			"ruff",
+	  			"pyright",
+				}
+      }
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {},
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.pyright.setup {
+				{
+					python = {
+						analysis = {
+							autoSearchPaths = true,
+							diagnosticMode = "openFilesOnly",
+							useLibraryCodeForTypes = true
+						}
+					}
+				}
+			}
+			lspconfig.rust_analyzer.setup {}
+			lspconfig.tsserver.setup {}
+		end
+  }
 }
