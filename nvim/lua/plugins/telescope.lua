95856aaac638b0f1034528e385e65b68f18ff5a4
gabriel torres <gabrieltggv29@gmail.com>
2024-01-30 16:48:52 -0300

    Adicionado o comando para deletar o buffer

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,7 +3,16 @@ return {
   tag='0.1.5',
   dependencies = {'nvim-lua/plenary.nvim'},
   config = function()
-    require("telescope").setup()
+		local actions = require('telescope.actions')
+    require("telescope").setup({
+			defaults = {
+				mappings = {
+					i = {
+						['<C-d>'] = actions.delete_buffer, 
+					}
+				}
+			}
+		})
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
