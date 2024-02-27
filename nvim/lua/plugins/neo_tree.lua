6761d90646dceb1edfa3a1dcb4e6ffe9fe732424
gabriel torres <gabrieltggv29@gmail.com>
2024-02-27 12:26:42 -0300

    Adjusted neo-tree with tabs

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -3,7 +3,7 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
@@ -11,15 +11,24 @@ return {
 			window = {
 				width = 30,
 			},
-			source_switcher = {
-				enable = true,
-				tabs_default = { icon = "裡", hl = "NeoTreeTitleBar", hl_active = "NeoTreeModified" },
-				tabs = { -- could be table or string (=icon), missing keys fall back to `tabs_default`
-					filesystem = { icon = "", text = "Files", hl = "ErrorMsg" },
-					buffers = "", -- no need to specify all sources (will use default then)
-					git_status = "",
+			source_selector = {
+				winbar = true,
+				statusline = false,
+				show_scrolled_off_parent_node = false,
+				sources = { -- table
+					{
+						source = "filesystem",
+						display_name = " 󰉓 Files ",
+					},
+					{
+						source = "buffers",
+						display_name = " 󰈚 Buffers ",
+					},
+					{
+						source = "git_status",
+						display_name = " 󰊢 Git ",
+					},
 				},
-				justify = "center",
 			},
 		})
 	end,
