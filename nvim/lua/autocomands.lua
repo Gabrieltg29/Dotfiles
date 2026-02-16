8ccf7e3d6f599bd9d45d47afb3487ff6a08a9fdb
Gabriel Torres <gabriel.gomes@mtmtecnologia.com.br>
2026-02-16 07:23:39 -0300

    fix(autocmd): use FileType event for indentation group

--- nvim/lua/autocomands.lua
+++ nvim/lua/autocomands.lua
@@ -2,7 +2,7 @@ local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
 local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 augroup("setIndent", { clear = true })
-autocmd("Filetype", {
+autocmd("FileType", {
 	group = "setIndent",
 	pattern = {
 		"css",
@@ -17,6 +17,19 @@ autocmd("Filetype", {
 		"xhtml",
 		"yaml",
 		"lua",
+		"json",
+		"svelte",
 	},
 	command = "setlocal shiftwidth=2 tabstop=2",
+})
+
+autocmd("LspAttach", {
+	callback = function(args)
+		if args.data and args.data.client_id then
+			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			if client and client.server_capabilities.inlayHintProvider then
+				vim.lsp.inlay_hint.enable(true)
+			end
+		end
+	end
 })
