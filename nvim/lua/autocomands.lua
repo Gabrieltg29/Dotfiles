33116b0d3b17d65a05cde6db9626495dfa20f173
Gabriel Torres <gabriel.gomes@mtmtecnologia.com.br>
2026-02-16 07:38:46 -0300

    feat(autocmd): improve workflow defaults for ide usage

--- nvim/lua/autocomands.lua
+++ nvim/lua/autocomands.lua
@@ -8,10 +8,12 @@ autocmd("FileType", {
 		"css",
 		"html",
 		"javascript",
+		"javascriptreact",
 		"lua",
 		"markdown",
 		"md",
 		"typescript",
+		"typescriptreact",
 		"scss",
 		"xml",
 		"xhtml",
@@ -19,6 +21,7 @@ autocmd("FileType", {
 		"lua",
 		"json",
 		"svelte",
+		"vue",
 	},
 	command = "setlocal shiftwidth=2 tabstop=2",
 })
@@ -32,4 +35,60 @@ autocmd("LspAttach", {
 			end
 		end
 	end
+})
+
+augroup("ideWorkflow", { clear = true })
+
+autocmd("TextYankPost", {
+	group = "ideWorkflow",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 180 })
+	end,
+})
+
+autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+	group = "ideWorkflow",
+	command = "checktime",
+})
+
+autocmd("BufReadPost", {
+	group = "ideWorkflow",
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local line_count = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= line_count then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
+
+autocmd("FileType", {
+	group = "ideWorkflow",
+	pattern = { "gitcommit", "markdown", "text" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+autocmd("FileType", {
+	group = "ideWorkflow",
+	pattern = {
+		"help",
+		"man",
+		"qf",
+		"lspinfo",
+		"checkhealth",
+		"startuptime",
+		"tsplayground",
+	},
+	callback = function(event)
+		vim.bo[event.buf].buflisted = false
+		vim.keymap.set("n", "q", "<Cmd>close<CR>", {
+			buffer = event.buf,
+			noremap = true,
+			silent = true,
+			desc = "Close window",
+		})
+	end,
 })
