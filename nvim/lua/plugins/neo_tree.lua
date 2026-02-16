03b245fa9da396cfe8f4616950eab27a9c7f482c
Gabriel Torres <gabriel.gomes@mtmtecnologia.com.br>
2026-02-16 08:05:30 -0300

    fix(neo-tree): open Neo-tree when starting with directory

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -2,6 +2,20 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
 	cmd = "Neotree",
+	init = function()
+		-- Prefer Neo-tree over netrw when opening a directory (e.g. `nvim .`)
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				local arg = vim.fn.argv(0)
+				if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+					vim.cmd("Neotree current dir=" .. vim.fn.fnameescape(arg))
+				end
+			end,
+		})
+	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
@@ -9,6 +23,9 @@ return {
 	},
 	config = function()
 		require("neo-tree").setup({
+			filesystem = {
+				hijack_netrw_behavior = "open_default",
+			},
 			window = {
 				width = 30,
 			},
